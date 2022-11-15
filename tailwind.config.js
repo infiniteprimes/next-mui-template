566c5b81dc85d2594b59807db7f700f53e794278
rick <abneryaoo@gmail.com>
2022-11-15 21:46:22 +0800

    use mantine

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./component/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './component/**/*.{js,ts,jsx,tsx}',
     './module/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    colors: {
+      primary: '#064e3b',
+      secondary: '#2196f3',
+      warning: '#FFB300',
+      error: '#ef5350',
+      darkBackground: '#393939',
+    },
     extend: {},
   },
   plugins: [],
